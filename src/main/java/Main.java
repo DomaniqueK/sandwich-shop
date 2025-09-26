8385879ee7e793fb05a437f71f185d712faa61ca
DomaniqueK <domaniqueking24@gmail.com>
2025-09-25 20:38:57 -0500

    Added a loaded option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,11 +14,12 @@ public class Main {
         else if (input.equals("2"))
             basePrice = largeBase;
 
-        System.out.println("How old are you? Are you a Student (17 or younger) or a Senior (65 or older");
+        System.out.println("How old are you? Are you a Student (17 or younger) or a Senior (65 or older?");
         double student = .10; // 10% discount
         double senior = .20; // 20% discount
         double discount; // final discount if any
         int age = myScanner.nextInt();
+        myScanner.nextLine();
         if (age <= 17) {
             discount = basePrice * student;
             System.out.println("You get a %10 discount!");
@@ -31,7 +32,23 @@ public class Main {
             discount = 0;
             System.out.print("Unfortunately, you get nothing.");
         }
-        double totalPrice = basePrice + discount;
+
+        System.out.println("Would you like the sandwich loaded? Option 1: $1.00 for a regular and Option 2: $1.75 for a large.");
+        input = myScanner.nextLine();
+        double loadedR = 1.00;
+        double loadedL = 1.75;
+        double loaded;
+        if (input.equals("1")) {
+            loaded = loadedR;
+        }
+        else if (input.equals("2")) {
+            loaded = loadedL;
+        }
+        else {
+            loaded = 0.0;
+        }
+
+        double totalPrice = basePrice + loaded + discount;
 
         System.out.printf(" Total cost of your sandwich is, $%.2f",  totalPrice);
     }
